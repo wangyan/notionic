013f38fb779df48a2d1d71b157b7fe7aad5782d8
WangYan <service@wangyan.org>
2024-08-11 12:39:24 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   transpilePackages: ['dayjs'],
   images: {
-    domains: ['api.craft.do', 'www.notion.so', 'images.unsplash.com', 'img.wangyan.org', 'img.cdm.wangyan.org']
+    domains: ['api.craft.do', 'www.notion.so', 'images.unsplash.com', 'img.cdn.wangyan.org', 'img.cdn.wangyan.cloud']
   },
   async headers() {
     return [
