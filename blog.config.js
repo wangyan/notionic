b72e342b225e05aba00a8c971b7973485d623638
WangYan <service@wangyan.org>
2024-08-11 18:22:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
     friends: true
   },
   showWeChatPay: false,
-  previewImagesEnabled: false,
+  previewImagesEnabled: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
