e42ccc14945a29419f62b25602b1ca445fd1acc8
WangYan <service@wangyan.org>
2024-08-11 18:37:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   darkBackground: '#212936', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Notionic in a folder
   since: 2024, // If leave this empty, current year will be used.
-  postsPerPage: 10,
+  postsPerPage: 15,
   sortByDate: true,
   pagesShow: {
     newsletter: true,
