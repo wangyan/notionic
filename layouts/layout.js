400e337aeca483f41600aa93814fe638395db61c
WangYan <service@wangyan.org>
2024-08-11 12:51:34 +0000

    Update layout.js

--- layouts/layout.js
+++ layouts/layout.js
@@ -7,7 +7,6 @@ import Content from '@/components/Post/Content'
 import Aside from '@/components/Post/Aside'
 import Comments from '@/components/Post/Comments'
 import PostFooter from '@/components/Post/PostFooter'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const Layout = ({ blockMap, frontMatter, fullWidth = false, subPage = false }) => {
   const [showSubPageTitle, setShowSubPageTitle] = useState(false)
@@ -40,7 +39,6 @@ const Layout = ({ blockMap, frontMatter, fullWidth = false, subPage = false }) =
         />
       </motion.div>
       <PostFooter />
-      <SpeedInsights />
       <Comments frontMatter={frontMatter} />
     </Container>
   )
