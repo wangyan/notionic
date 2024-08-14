2abe35b5b806067744f984df8d4f18d6be1c01f7
WangYan <service@wangyan.org>
2024-08-14 11:21:38 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -4,8 +4,8 @@ import * as React from 'react'
 const Logo = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={24}
-    height={24}
+    width={20}
+    height={20}
     viewBox="0 0 26 26"
     {...props}
   >
